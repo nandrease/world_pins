a10f74c2c5f5fd266910323d8a04244f377f856f
nandrease <neeme.andreas.eller@gmail.com>
2019-08-08 17:58:48 +0300

    Add fonts

--- script.js
+++ script.js
@@ -1,135 +1,135 @@
 let pinLocations = [];
-      fetch("data.json")
-        .then(response => response.json())
-        .then(parsed => {
-          pinLocations = parsed;
-          slide1();
-        });
-      const kaart = document.querySelector(".map");
-      const pinsLayer = kaart.querySelector(".pinsLayer");
-      const cityLayer = kaart.querySelector(".cityLayer");
-      const infoLayer = kaart.querySelector(".infoLayer");
-      const pin = document.querySelector(".pin");
-      let slideIndex = 0;
+fetch("data.json")
+  .then(response => response.json())
+  .then(parsed => {
+    pinLocations = parsed;
+    slide1();
+  });
+const kaart = document.querySelector(".map");
+const pinsLayer = kaart.querySelector(".pinsLayer");
+const cityLayer = kaart.querySelector(".cityLayer");
+const infoLayer = kaart.querySelector(".infoLayer");
+const pin = document.querySelector(".pin");
+let slideIndex = 0;
 
-      function addPin(loc) {
-        const { coords, id, city, info } = loc;
-        let node = pin.cloneNode(true);
-        if (city) {
-          node.addEventListener("click", e => console.log(city));
-        }
-        if (info) {
-          node.addEventListener("click", e => console.log(info));
-        }
-        node.addEventListener("click", e => console.log(id));
-        const css = `
+function addPin(loc, id=0, delay=50) {
+  const { coords, city, info } = loc;
+  let node = pin.cloneNode(true);
+  if (city) {
+    node.addEventListener("click", e => console.log(city));
+  }
+  if (info) {
+    node.addEventListener("click", e => console.log(info));
+  }
+  node.addEventListener("click", e => console.log(id));
+  const css = `
           position:absolute;
           left: ${coords.x}px;
           top: ${coords.y}px;
-          animation-delay: ${id * 50 + 200}ms;
+          animation-delay: ${id * delay + 200}ms;
           `;
-        node.style = css;
-        pinsLayer.appendChild(node);
-      }
+  node.style = css;
+  pinsLayer.appendChild(node);
+}
 
-      function addCityBox(loc) {
-        const { city, coords, left } = loc;
-        let node = document.createElement("div");
-        node.classList.add("city");
-        left && node.classList.add("left");
-        node.textContent = city;
-        const css = `
+function addCityBox(loc,id=0, delay=80) {
+  const { city, coords, left } = loc;
+  let node = document.createElement("div");
+  node.classList.add("city");
+  left && node.classList.add("left");
+  node.textContent = city;
+  const css = `
               position:absolute;
               left: ${coords.x}px;
               top: ${coords.y}px;
+              animation-delay: ${id * delay + 200}ms;
             `;
-        node.style = css;
-        cityLayer.appendChild(node);
-      }
+  node.style = css;
+  cityLayer.appendChild(node);
+}
 
-      function addInfoBox(loc) {
-        const { title, info } = loc;
-        let node = document.createElement("div");
-        node.classList.add("info");
-        title.left && node.classList.add("left");
-        node.innerHTML = `
+function addInfoBox(loc, id=0, delay=50) {
+  const { title, info } = loc;
+  let node = document.createElement("div");
+  node.classList.add("info");
+  title.left && node.classList.add("left");
+  node.innerHTML = `
           <h3>Perfomance</h3>
           <table>
               <tr>
                   <td>Visitors last week</td>
                   <td><strong>${info.lastWeek.total}</strong> <span class="${
-          info.lastWeek.gain >= 0 ? "pos" : "neg"
-        }">(${info.lastWeek.gain >= 0 ? "+" : ""}${
-          info.lastWeek.gain
-        }%)</span></td>
+    info.lastWeek.gain >= 0 ? "pos" : "neg"
+  }">(${info.lastWeek.gain >= 0 ? "+" : ""}${info.lastWeek.gain}%)</span></td>
               </tr>
               <tr>
                   <td>Wait time</td>
                   <td><strong>${info.waitTime.time}min</strong> <span class="${
-          info.waitTime.gain < 0 ? "pos" : "neg"
-        }">(${info.waitTime.gain >= 0 ? "+" : ""}${
-          info.waitTime.gain
-        }%)</span></td>
+    info.waitTime.gain < 0 ? "pos" : "neg"
+  }">(${info.waitTime.gain >= 0 ? "+" : ""}${info.waitTime.gain}%)</span></td>
               </tr>
               <tr>
                   <td>Service time</td>
                   <td><strong>${
                     info.serviceTime.time
                   }min</strong> <span class="${
-          info.serviceTime.gain < 0 ? "pos" : "neg"
-        }">(${info.serviceTime.gain >= 0 ? "+" : ""}${
-          info.serviceTime.gain
-        }%)</span></td>
+    info.serviceTime.gain < 0 ? "pos" : "neg"
+  }">(${info.serviceTime.gain >= 0 ? "+" : ""}${
+    info.serviceTime.gain
+  }%)</span></td>
               </tr>
           </table>
           `;
-        const css = `
+  const css = `
               position:absolute;
               left: ${title.coords.x}px;
               top: ${title.coords.y}px;
+              animation-delay: ${id * delay + 200}ms;
             `;
-        node.style = css;
-        infoLayer.appendChild(node);
-      }
+  node.style = css;
+  infoLayer.appendChild(node);
+}
 
-      function nextSlide() {
-        slideIndex >= 2 ? (slideIndex = 0) : slideIndex++;
-        switch (slideIndex) {
-          case 1:
-            slide2();
-            break;
-          case 2:
-            cityLayer.innerHTML = "";
-            slide3();
-            break;
-          default:
-            infoLayer.innerHTML = "";
-            cityLayer.innerHTML = "";
-            pinsLayer.innerHTML = "";
-            slide1();
-            return;
-        }
-      }
+function nextSlide() {
+  slideIndex >= 2 ? (slideIndex = 0) : slideIndex++;
+  switch (slideIndex) {
+    case 1:
+      slide2();
+      break;
+    case 2:
+      cityLayer.innerHTML = "";
+      slide3();
+      break;
+    default:
+      infoLayer.innerHTML = "";
+      cityLayer.innerHTML = "";
+      pinsLayer.innerHTML = "";
+      slide1();
+      return;
+  }
+}
 
-      function slide1() {
-        pinLocations.forEach(loc => {
-          addPin(loc);
-        });
-      }
+function slide1() {
+  pinLocations.forEach(loc => {
+    addPin(loc, loc.id);
+  });
+}
 
-      function slide2(e) {
-        const cityNames = pinLocations.filter(pin => pin.title);
-        cityNames.forEach(pin => {
-          addCityBox(pin.title);
-        });
-      }
+function slide2(e) {
+  const cityNames = pinLocations.filter(pin => pin.title);
+  cityNames.forEach((pin, idx) => {
+    addCityBox(pin.title, idx);
+  });
+}
 
-      function slide3(e) {
-        const cityNames = pinLocations.filter(pin => pin.info);
-        cityNames.forEach(loc => {
-          addCityBox(loc.title);
-          addInfoBox(loc);
-        });
-      }
+function slide3(e) {
+  const cityNames = pinLocations.filter(pin => pin.info);
+  cityNames.forEach((loc, idx) => {
+    addCityBox(loc.title);
+    addInfoBox(loc, idx);
+  });
+}
 
-      kaart.addEventListener("click", nextSlide);
+kaart.addEventListener("click", nextSlide);
+
+setInterval(function() {nextSlide()}, 5000);
